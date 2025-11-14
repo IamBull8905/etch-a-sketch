98ff794961c5c7286f5598b78d7c7e9ca0331f3f
IamBull8905 <217627756+IamBull8905@users.noreply.github.com>
2025-11-14 20:08:26 +0000

    Create a 16x16 grid of divs using js & apply basic styling

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+let rows = 16;
+let columns = 16;
+
+for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("tile");
+        container.appendChild(newDiv);
+    };
+}
