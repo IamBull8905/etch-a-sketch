9cd9860a2b7543a7a2665fe7ca686599bd056897
IamBull8905 <217627756+IamBull8905@users.noreply.github.com>
2025-11-16 19:44:24 +0000

    Add event listeners to each tile for the mouse "hover" effect

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ for (let i = 0; i < rows; i++) {
     for (let j = 0; j < columns; j++) {
         const newDiv = document.createElement("div");
         newDiv.classList.add("tile");
+        newDiv.addEventListener("mouseover", () => newDiv.style.backgroundColor = "#7851A9");
         container.appendChild(newDiv);
     };
 }
