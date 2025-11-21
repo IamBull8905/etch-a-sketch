0c9a368b6a1705d3fe50e74d9bc0787be035749d
IamBull8905 <217627756+IamBull8905@users.noreply.github.com>
2025-11-21 20:16:14 +0000

    Add the button that changes the amount of tiles in the grid

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const container = document.querySelector(".container");
-let rows = 16;
-let columns = 16;
+const button = document.querySelector("button");
+let size = 16;
 
-for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < columns; j++) {
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("tile");
-        newDiv.addEventListener("mouseover", () => newDiv.style.backgroundColor = "#7851A9");
-        container.appendChild(newDiv);
+function setGridSize(size) {
+    container.innerHTML = "";
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            const newDiv = document.createElement("div");
+            newDiv.classList.add("tile");
+            newDiv.addEventListener("mouseover", () => newDiv.style.backgroundColor = "#7851A9");
+            container.appendChild(newDiv);
+        };
     };
-}
+};
+
+setGridSize(size);
+
+button.addEventListener("click", () => {
+    let userInput = Number(prompt("Please enter the dimensions of the grid you desire (max 100x100): "));
+    while (isNaN(userInput)) {
+        userInput = Number(prompt("Enter a valid number please!"));
+    };
+    if (userInput > 100 || userInput <= 0) {
+        console.log("Invalid input");
+    } else {
+        size = userInput;
+    };
+    setGridSize(size);
+});
