6cca853b3cc93152233c124f1bea6917e06f87dc
IamBull8905 <217627756+IamBull8905@users.noreply.github.com>
2025-11-21 22:08:09 +0000

    Add functionality so the tiles flex within the container

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ const container = document.querySelector(".container");
 const button = document.querySelector("button");
 let size = 16;
 
-function setGridSize(size) {
-    container.innerHTML = "";
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            const newDiv = document.createElement("div");
-            newDiv.classList.add("tile");
-            newDiv.addEventListener("mouseover", () => newDiv.style.backgroundColor = "#7851A9");
-            container.appendChild(newDiv);
-        };
+for (let i = 0; i < 32; i++) {
+    let rowDiv = document.createElement("div");
+    rowDiv.classList.add("row-div");
+    for (let j = 0; j < 32; j++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("tile");
+        newDiv.addEventListener("mouseover", () => newDiv.style.backgroundColor = "#7851A9");
+        rowDiv.appendChild(newDiv);
     };
+    container.appendChild(rowDiv);
 };
 
-setGridSize(size);
+
+
+// setGridSize(size);
 
 button.addEventListener("click", () => {
     let userInput = Number(prompt("Please enter the dimensions of the grid you desire (max 100x100): "));
